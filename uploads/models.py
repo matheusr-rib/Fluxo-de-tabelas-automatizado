1465c4262276fb1a22b31dac1c7236119e3e0426
MATHEUS REIS RIBEIRO <matheus.r@lewecp.local>
2025-09-15 15:19:43 -0300

    estrutura inical

--- uploads/models.py
+++ uploads/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Upload(models.Model):
+    arquivo = models.FileField(upload_to='')
+    criado_em = models.DateTimeField(auto_now_add=True)
--- uploads/views.py
+++ uploads/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import os
+from datetime import datetime
+from django.shortcuts import render, redirect
+from .forms import UploadForm
+import openpyxl
 
-# Create your views here.
+EXCEL_PATH = r'Z:\PRICING\Matheus\PROJETOFLUXOTABELAS\comissoes.xlsx'
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            upload = form.save()
+
+
+            data = datetime.now().strftime("%d/%m/%Y")
+            hora = datetime.now().strftime("%H:%M:%S")
+            nome = upload.arquivo.name
+            caminho = os.path.join(r'\\servidor\arquivos_banco', nome)
+
+    
+            wb = openpyxl.load_workbook(EXCEL_PATH)
+            ws = wb.active
+            ws.append([data, hora, nome, caminho])  
+            wb.save(EXCEL_PATH)
+
+            return redirect('upload_sucesso')
+    else:
+        form = UploadForm()
+    return render(request, 'upload.html', {'form': form})
